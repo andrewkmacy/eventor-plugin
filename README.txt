889095f6af409a7d27b301a87848993974aaabf9
Andrew K Macy <andrewkmacy@gmail.com>
2020-02-14 12:27:45 -0500

    add critical files and folders

---
+++ README.txt
@@ -0,0 +1,11 @@
+=== Eventor ===
+Contributors: @drewbiwan (https://profiles.wordpress.org/drewbiwan/)
+Tags: events
+Requires at least: 5.1
+Tested up to: 5.2
+Requires PHP: 7.2
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Eventor is a lightweight events plugin for WordPress. 
---
+++ inc/form.php
@@ -0,0 +1,37 @@
+<div class="eventor_box">
+    <style scoped>
+        .eventor_box{
+            display: inline-block;
+            grid-template-columns: max-content 1fr;
+            grid-row-gap: 10px;
+            grid-column-gap: 20px;
+        }
+        .eventor_field{
+            display: contents;
+        }
+    </style>
+    <p class="meta-options eventor_date">
+        <label for="eventor_time_set">Event Date</label>
+        <input 
+            id="eventor_date_set" 
+            type="date" 
+            name="eventor_date_set"
+            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_date_set', true ) ); ?>">
+    </p>
+    <p class="meta-options eventor_time">
+        <label for="eventor_time_set">Event Time</label>
+        <input 
+            id="eventor_time_set" 
+            type="time" 
+            name="eventor_time_set"
+            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_time_set', true ) ); ?>">
+    </p>
+    <p class="meta-options eventor_location">
+        <label for="eventor_location_set">Event Location</label>
+        <input 
+            id="eventor_location_set" 
+            type="text" 
+            name="eventor_location_set"
+            value="<?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_location_set', true ) ); ?>">
+    </p>
+</div>
