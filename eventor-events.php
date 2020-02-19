4c1c65c39e97b0692463b8e3a66a0eca4fe07cb3
Andrew K Macy <andrewkmacy@gmail.com>
2020-02-19 08:50:30 -0500

    move metabox functions to separate file, call file inside __construct in Event class, organize file locations

--- eventor-events.php
+++ eventor-events.php
@@ -15,6 +15,7 @@ if (!defined('ABSPATH'))
 class Event {
 
 	public function __construct (){
+		include plugin_dir_path( __FILE__ ) . './inc/event-meta.php';
 		add_action('init', array($this, 'register'));
 	}
 	// Register Custom Post Type
@@ -76,72 +77,3 @@ class Event {
 
 $Event = new Event();
 
-function events_add_meta_boxes() {
-
-  add_meta_box( 
-  'eventor_details_set', 
-  'Event Details', 
-  'eventor_details_set',
-  'eventor',
-  'side',
-  'high',
-  'null'
-  );
-
-}
-add_action( 'add_meta_boxes_eventor', 'events_add_meta_boxes' );
-
-/**
-* Output the HTML for the metabox.
-*/
-
-function eventor_details_set() {
-global $post;
-include plugin_dir_path( __FILE__ ) . './inc/form.php';
-wp_nonce_field( basename( __FILE__ ), 'eventor_details_set' );
-
-$date = get_post_meta( $post->ID, 'Event Details', true );
-
-}
-
-/**
- * Save meta box content.
- *
- * @param int $post_id Post ID
- */
-function eventor_save_meta_box( $post_id ) {
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-    if ( $parent_id = wp_is_post_revision( $post_id ) ) {
-        $post_id = $parent_id;
-    }
-    $fields = [
-		'eventor_date_set',
-        'eventor_time_set',
-        'eventor_location_set',
-    ];
-    foreach ( $fields as $field ) {
-        if ( array_key_exists( $field, $_POST ) ) {
-            update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
-        }
-     }
-}
-add_action( 'save_post', 'eventor_save_meta_box' );
-
-/*
- * Add page templates
- */
-
-add_filter( 'template_include', 'eventor_templates' );
-function eventor_templates( $template ) {
-    $post_types = array( 'eventor' );
-
-    if ( is_post_type_archive( $post_types ) && file_exists( plugin_dir_path(__FILE__) . 'templates/eventor-archive.php' ) ){
-        $template = plugin_dir_path(__FILE__) . 'templates/eventor-archive.php';
-    }
-
-    if ( is_singular( $post_types ) && file_exists( plugin_dir_path(__FILE__) . 'templates/eventor-single.php' ) ){
-        $template = plugin_dir_path(__FILE__) . 'templates/eventor-single.php';
-    }
-
-    return $template;
-}
