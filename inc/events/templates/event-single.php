49db92810a6f5fed79c2eedba3cfb98679a8ecd8
Andrew K Macy <andrewkmacy@gmail.com>
2020-02-20 14:31:18 -0500

    re-add event template files

---
+++ inc/events/templates/event-single.php
@@ -0,0 +1,40 @@
+<?php
+
+get_header(); ?>
+
+<div id="main">
+	
+	<section id="content">
+		
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			
+            <h1><?php the_title(); ?></h1>
+            <!-- show event info -->
+                <ul>
+                    <li><strong>Date: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_date_set', true ) ); ?></li>
+                    <li><strong>Time: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_time_set', true ) ); ?></li>
+                    <li><strong>Location: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_location_set', true ) ); ?></li>
+                </ul>
+			
+			<?php the_content(); ?>
+									
+			<nav class="post-navigation">
+				<div class="nav-previous"><?php previous_post_link( '&laquo; %link' ); ?></div>
+				<div class="nav-next"><?php next_post_link( '%link &raquo;' ); ?></div>
+			</nav>
+			
+		</article>
+
+	  <?php comments_template(); ?>
+	
+	  <?php endwhile; endif; ?>
+	
+	</section>
+	
+<?php get_sidebar(); ?>
+
+</div><!-- end of main div -->
+
+<?php get_footer(); ?>
---
+++ inc/templates/event-archive.php
@@ -0,0 +1,40 @@
+<?php
+/* 
+Template Name: Archives
+*/
+get_header(); ?>
+    <header class="page-header" role="banner">
+		<?php
+		the_archive_title( '<h1 class="page-title">', '</h1>' );
+		the_archive_description( '<div class="archive-description">', '</div>' );
+		?>
+    </header><!-- .page-header -->
+
+ 
+<div id="primary" class="site-content">
+<div id="content" role="main">
+ 
+<?php while ( have_posts() ) : the_post(); ?>
+                 
+<?php the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' ); ?>
+
+<!-- show event info -->
+<ul class="events">
+    <li><strong>Date: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_date_set', true ) ); ?></li>
+    <li><strong>Time: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_time_set', true ) ); ?></li>
+    <li><strong>Location: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_location_set', true ) ); ?></li>
+</ul>
+ 
+<div class="entry-content">
+ 
+<?php the_content(); ?>
+ 
+</div><!-- .entry-content -->
+ 
+<?php endwhile; // end of the loop. ?>
+ 
+</div><!-- #content -->
+</div><!-- #primary -->
+ 
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
