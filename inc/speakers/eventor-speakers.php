89ab96b876d80c57dd576e4962e7cb554532885f
Andrew K Macy <andrewkmacy@gmail.com>
2020-02-20 13:04:53 -0500

    remove template files as Speaker custom post type does not add new meta data

---
+++ inc/speakers/eventor-speakers.php
@@ -0,0 +1,69 @@
+<?php
+
+// Exit if accessed directly
+if (!defined('ABSPATH'))
+  exit;
+
+class Speaker {
+
+	public function __construct (){
+		add_action('init', array($this, 'eventor_speaker'));
+	}
+	// Register Custom Post Type
+	public function eventor_speaker() {
+
+		$labels = array(
+			'name'                  => 'Speakers',
+			'singular_name'         => 'Speaker',
+			'menu_name'             => 'Speakers',
+			'name_admin_bar'        => 'Speakers',
+			'archives'              => 'Item Archives',
+			'attributes'            => 'Item Attributes',
+			'parent_item_colon'     => 'Parent Item:',
+			'all_items'             => 'All Items',
+			'add_new_item'          => 'Add New Item',
+			'add_new'               => 'Add New',
+			'new_item'              => 'New Item',
+			'edit_item'             => 'Edit Item',
+			'update_item'           => 'Update Item',
+			'view_item'             => 'View Item',
+			'view_items'            => 'View Items',
+			'search_items'          => 'Search Item',
+			'not_found'             => 'Not found',
+			'not_found_in_trash'    => 'Not found in Trash',
+			'featured_image'        => 'Featured Image',
+			'set_featured_image'    => 'Set featured image',
+			'remove_featured_image' => 'Remove featured image',
+			'use_featured_image'    => 'Use as featured image',
+			'insert_into_item'      => 'Insert into item',
+			'uploaded_to_this_item' => 'Uploaded to this item',
+			'items_list'            => 'Items list',
+			'items_list_navigation' => 'Items list navigation',
+			'filter_items_list'     => 'Filter items list',
+		);
+		$args = array(
+			'label'                 => 'Speaker',
+			'description'           => 'Custom post type for Speakers',
+			'labels'                => $labels,
+			'supports'              => array( 'title', 'editor', 'thumbnail' ),
+			'taxonomies'            => array( 'category', 'post_tag' ),
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 5,
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => true,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
+			'capability_type'       => 'page',
+		);
+	
+	register_post_type( 'speaker', $args );
+
+	}
+}
+
+$Speaker = new Speaker();
