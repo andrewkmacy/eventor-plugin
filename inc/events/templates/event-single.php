76812271be88e1631507d1457651a3b6b2b90f07
Andrew K Macy <andrewkmacy@gmail.com>
2020-02-26 16:50:50 -0500

    added wrapper style div outside entire post, added div to contain all actual content up to post nav

--- inc/events/templates/event-single.php
+++ inc/events/templates/event-single.php
@@ -1,36 +1,36 @@
 <?php
 
 get_header(); ?>
-
 	<!-- main content area -->
-	<div class="event_content">
+	<div class="event_wrapper">
 		
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
+
+			<div class="event_content">
+
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					
 					<h1><?php the_title(); ?></h1>
-
 							<!-- show event info -->	
 							<ul>			
 								<li><strong>Date: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_date_set', true ) ); ?></li>
 								<li><strong>Time: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_time_set', true ) ); ?></li>
 								<li><strong>Location: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_location_set', true ) ); ?></li>
-							</ul>
-							
+							</ul>				
 							<!-- add text content -->
 					<?php the_content(); ?>
-						
-					<nav class="post-navigation">
-						<div class="nav-previous"><?php previous_post_link( '&laquo; %link' ); ?></div>
-						<div class="nav-next"><?php next_post_link( '%link &raquo;' ); ?></div>
-					</nav>
+
+				</article>	
+				<!-- add featured image -->
+				<div class="event_image"> <?php the_post_thumbnail(); ?></div>
+
+			</div>
+
+		<nav class="post-navigation">
+			<div class="nav-previous"><?php previous_post_link( '&laquo; %link' ); ?></div>
+			<div class="nav-next"><?php next_post_link( '%link &raquo;' ); ?></div>
+		</nav>
 					
-				</article>
-
-		<!-- add featured image -->
-		<div class="event_image"> <?php the_post_thumbnail(); ?></div>
-
 		<?php comments_template(); ?>		
 		<?php endwhile; endif; ?>
 	
