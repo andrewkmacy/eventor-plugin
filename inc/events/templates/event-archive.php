6e6782c4bc454593bfc1b20cc4ac7bfb4d280f79
Andrew K Macy <andrewkmacy@gmail.com>
2020-02-26 18:06:34 -0500

    added outer wrapper to, consolidated redundant classes in archive template

--- inc/events/templates/event-archive.php
+++ inc/events/templates/event-archive.php
@@ -1,50 +1,41 @@
+
 <?php
 /* 
 Template Name: Archives
 */
 get_header(); ?>
-    <header class="page-header" role="banner">
-      <h1><?php wp_title(); ?></h1>
-    </header><!-- .page-header -->
+  <header class="page-header" role="banner">
+    <h1><?php wp_title(); ?></h1>
+  </header><!-- .page-header -->
 
-    <div class="archive_event_main">
-      <!-- main content area -->
-      <div class="archive_event_content">
+<div class="archive_main">
+		
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <div class="archive_content">
 
-          <div class="archive_content_wrapper">
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            
+          <h1><?php the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' ); ?></h1>
+            <!-- show event info -->	
+            <ul>			
+              <li><strong>Date: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_date_set', true ) ); ?></li>
+              <li><strong>Time: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_time_set', true ) ); ?></li>
+              <li><strong>Location: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_location_set', true ) ); ?></li>
+            </ul>				
+            <!-- add text content -->
+          <?php the_content(); ?>
 
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-              
-              <h1><?php the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' ); ?></h1>
-
-                  <!-- show event info -->	
-                  <ul>			
-                    <li><strong>Date: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_date_set', true ) ); ?></li>
-                    <li><strong>Time: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_time_set', true ) ); ?></li>
-                    <li><strong>Location: </strong><?php echo esc_attr( get_post_meta( get_the_ID(), 'eventor_location_set', true ) ); ?></li>
-                  </ul>
-                  
-                  <!-- add text content -->
-              <?php the_content(); ?>
-                
-              <nav class="post-navigation">
-                <div class="nav-previous"><?php previous_post_link( '&laquo; %link' ); ?></div>
-                <div class="nav-next"><?php next_post_link( '%link &raquo;' ); ?></div>
-              </nav>
-              
-            </article>
-
+        </article>	
             <!-- add featured image -->
-            <div class="archive_event_image"> <?php the_post_thumbnail(); ?></div>
+        <div class="event_image"> <?php the_post_thumbnail(); ?></div>
+          <nav class="post-navigation">
+            <div class="nav-previous"><?php previous_post_link( '&laquo; %link' ); ?></div>
+            <div class="nav-next"><?php next_post_link( '%link &raquo;' ); ?></div>
+          </nav>
 
-          </div> <!-- single event -->
+  <?php endwhile; endif; ?>	
+  
+</div>
 
-            <?php endwhile; endif; ?>
-      
-      </div>
-
-    </div><!-- end of main div -->
-
-<?php get_footer(); ?>	
+<?php get_footer(); ?>
